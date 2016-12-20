490df8aaa829bb254c03cd20b8cd377b6f1ee341
Luke VanderHart <luke@vanderhart.net>
2016-12-20 09:37:34 -0500

    bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject arachne-framework/valuehash "0.1.0-SNAPSHOT"
+(defproject org.arachne-framework/valuehash "0.1.0"
   :description "Value based hashing for Clojure data"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [org.clojure/test.check "0.9.0" :scope "test"]
-                 [criterium "0.4.4" :scope "test"]])
+                 [criterium "0.4.4" :scope "test"]]
+  :repositories [["arachne-dev" "http://maven.arachne-framework.org/artifactory/arachne-dev"]])
