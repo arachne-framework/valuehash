5ef0a055e247f79e66e7f10b361f640fdd9b7e43
Luke VanderHart <luke@vanderhart.net>
2018-11-02 12:47:51 -0400

    convert to deps.edn

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject org.arachne-framework/valuehash "0.1.1"
-  :description "Value based hashing for Clojure data"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16" :scope "provided"]
-                 [org.clojure/test.check "0.9.0" :scope "test"]
-                 [criterium "0.4.4" :scope "test"]]
-  :repositories [["arachne-dev" "http://maven.arachne-framework.org/artifactory/arachne-dev"]])
